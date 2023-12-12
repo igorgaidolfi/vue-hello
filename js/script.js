e780628ecba7a6038de11a22cf4a0bbd54b627fc
igorgaidolfi <gandalf.4@outlook.it>
2023-12-12 15:43:41 +0100

    update Bonus completed

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ const { createApp } = Vue
 createApp({
     data(){
         return{
-            message: 'First message in Vue. Hello!'
+            message: 'First message in Vue. Hello!',
+            imageName: 'https://cdn.pixabay.com/photo/2018/01/14/14/45/la-rocher-de-la-baume-3081950_1280.jpg'
         }
     }
-}).mount('#app');
+}).mount('#hello');
